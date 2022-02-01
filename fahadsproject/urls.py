10d425b7c509faaae68d41279f88908929348e81
rafi <itzrayn178@gmail.com>
2022-02-01 12:02:00 +0600

    first comiin

---
+++ fahadsproject/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path,include,re_path
+from django.views.static import serve 
+
+
+urlpatterns = [
+    path('fahadZaman/', admin.site.urls),
+    path('',include('fahadsapp.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
